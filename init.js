a1cba3a5ede2b07d59abfea541938320249c7804
lee <zc.lee@tengnat.com>
2019-09-07 11:24:27 +0800

    v1.0.14

--- init.js
+++ init.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 const path = require('path')
-const l_push_global = require('./lib/push_global')
+const setGlobal = require('./lib/setGlobal')
 
 const obj = {
-    'echo': 'lzc-echo',
-    'throwError':path.join(__dirname,'./lib/throw_error.js')
+    echo: 'lzc-echo',
+    throwError: path.join(__dirname, './lib/throw_error.js'),
+    l_push_global: path.join(__dirname, './lib/setGlobal.js')
 }
-l_push_global(obj)
-let list=fs.readdirSync(path.join(__dirname,'/lib')).map(v=>path.parse(path.join(__dirname,'/lib',v)).name)
-l_push_global(list,path.join(__dirname,'/lib')+'/','l_')
+setGlobal(obj)
+
+let list = fs.readdirSync(path.join(__dirname, '/lib')).map(v => path.parse(path.join(__dirname, '/lib', v)).name)
+l_push_global(list, path.join(__dirname, '/lib') + '/', 'l_')
