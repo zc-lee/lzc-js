97d007d93f0a8c09dbbb4a98118f072a106ddd45
lee <zc.lee@tengnat.com>
2019-09-11 14:42:09 +0800

    v1.0.15

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 require('./index')
+
+var obj1 = {
+    a: 'a1',
+    obj: {
+        a: 'a1'
+    }
+}, obj2 = {
+    a: 'a2',
+    b: 'b2',
+    obj: {
+        a: 'a2',
+        b: 'b2',
+    }
+}
+Object.deepAssign(obj1, obj2)
+console.log(obj1)
+console.log(obj2)
